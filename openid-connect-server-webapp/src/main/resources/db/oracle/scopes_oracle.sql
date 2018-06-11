157c094ae2e0af01eb61f82a23e90ea9b6e4fcca
Angelo Kastroulis <angelo@kastroulis.com>
2018-06-11 15:50:05 -0400

    Fixed broken scripts from schema change

--- openid-connect-server-webapp/src/main/resources/db/oracle/scopes_oracle.sql
+++ openid-connect-server-webapp/src/main/resources/db/oracle/scopes_oracle.sql
@@ -2,18 +2,18 @@
 -- Insert scope information into the temporary tables.
 -- 
 
-INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('openid', 'log in using your identity', 'user', 0, 1, 0, null);
-INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('profile', 'basic profile information', 'list-alt', 0, 1, 0, null);
-INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('email', 'email address', 'envelope', 0, 1, 0, null);
-INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('address', 'physical address', 'home', 0, 1, 0, null);
-INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('phone', 'telephone number', 'bell', 0, 1, 0, null);
-INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('offline_access', 'offline access', 'time', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('openid', 'log in using your identity', 'user', 0, 1);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('profile', 'basic profile information', 'list-alt', 0, 1);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('email', 'email address', 'envelope', 0, 1);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('address', 'physical address', 'home', 0, 1);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('phone', 'telephone number', 'bell', 0, 1, 0);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('offline_access', 'offline access', 'time', 0, 0);
 
 INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
   ('smart', 'Details of SMART authorization', 'user', 0, 0, 0, null);
@@ -70,8 +70,8 @@ INSERT INTO system_scope (scope, description, icon, restricted, default_scope, s
 --
 
 MERGE INTO system_scope
-	USING (SELECT scope, description, icon, restricted, default_scope, structured, structured_param_description FROM system_scope_TEMP) vals
+	USING (SELECT scope, description, icon, restricted, default_scope FROM system_scope_TEMP) vals
 	ON (vals.scope = system_scope.scope)
 	WHEN NOT MATCHED THEN
-	  INSERT (id, scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES(system_scope_seq.nextval, vals.scope,
-	  vals.description, vals.icon, vals.restricted, vals.default_scope, vals.structured, vals.structured_param_description);
+	  INSERT (id, scope, description, icon, restricted, default_scope) VALUES(system_scope_seq.nextval, vals.scope,
+	  vals.description, vals.icon, vals.restricted, vals.default_scope);
