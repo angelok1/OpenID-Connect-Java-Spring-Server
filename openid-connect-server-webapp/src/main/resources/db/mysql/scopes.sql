5e099d6f16cb9237bc013b354344b977b585ce82
Angelo Kastroulis <angelo@kastroulis.com>
2018-06-11 15:50:27 -0400

    Added SMART on FHIR scopes to scripts

--- openid-connect-server-webapp/src/main/resources/db/mysql/scopes.sql
+++ openid-connect-server-webapp/src/main/resources/db/mysql/scopes.sql
@@ -18,32 +18,32 @@ INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_sco
   ('phone', 'telephone number', 'bell', false, true),
   ('offline_access', 'offline access', 'time', false, false),
   ('smart', 'Details of SMART authorization', 'user', false, false),
-  ('smart/orchestrate_launch', 'Orchestrate a launch with EHR context', 'user', false, false, false, null),
-  ('launch', 'Launch with an existing context', 'user', false, false, false, null),
-  ('launch/patient', 'Launch with patient context', 'user', false, false, false, null),
-  ('launch/encounter', 'Launch with encounter context', 'user', false, false, false, null),
-  ('launch/resource', 'Launch with resource context', 'user', false, false, false, null),
-  ('launch/other', 'Launch with other context', 'user', false, false, false, null),
-  ('search', 'SMART on FHIR search', 'user', false, false, false, null),
-  ('summary', 'SMART on FHIR summary', 'user', false, false, false, null),
-  ('user/*.*', 'Permission to read and write all resources that the current user can access', 'user', false, false, false, null),
-  ('patient/*.*', 'Permission to read/write any resource for the current patient', 'user', false, false, false, null),
-  ('patient/*.read', 'Read Permission to read any resource for the current patient', 'user', false, false, false, null),
-  ('patient/*.write', 'Write Permission to read any resource for the current patient', 'user', false, false, false, null),
-  ('patient/Patient/*.*', 'Read/Write to FHIR Patient Resources', 'user', false, false, false, null),
-  ('patient/Patient/*.read', 'Read to FHIR Patient Resources', 'user', false, false, false, null),
-  ('patient/Patient/*.write', 'Write to FHIR Patient Resources', 'user', false, false, false, null),
-  ('patient/Observation/*.*', 'Read/Write to FHIR Observations Resources', 'user', false, false, false, null),
-  ('patient/Observation/*.read', 'Read to FHIR Observations Resources', 'user', false, false, false, null),
-  ('patient/Observation/*.write', 'Write to FHIR Observations Resources', 'user', false, false, false, null);
+  ('smart/orchestrate_launch', 'Orchestrate a launch with EHR context', 'user', false, false),
+  ('launch', 'Launch with an existing context', 'user', false, false),
+  ('launch/patient', 'Launch with patient context', 'user', false, false),
+  ('launch/encounter', 'Launch with encounter context', 'user', false, false),
+  ('launch/resource', 'Launch with resource context', 'user', false, false),
+  ('launch/other', 'Launch with other context', 'user', false, false),
+  ('search', 'SMART on FHIR search', 'user', false, false),
+  ('summary', 'SMART on FHIR summary', 'user', false, false),
+  ('user/*.*', 'Permission to read and write all resources that the current user can access', 'user', false, false),
+  ('patient/*.*', 'Permission to read/write any resource for the current patient', 'user', false, false),
+  ('patient/*.read', 'Read Permission to read any resource for the current patient', 'user', false, false),
+  ('patient/*.write', 'Write Permission to read any resource for the current patient', 'user', false, false),
+  ('patient/Patient/*.*', 'Read/Write to FHIR Patient Resources', 'user', false, false),
+  ('patient/Patient/*.read', 'Read to FHIR Patient Resources', 'user', false, false),
+  ('patient/Patient/*.write', 'Write to FHIR Patient Resources', 'user', false, false),
+  ('patient/Observation/*.*', 'Read/Write to FHIR Observations Resources', 'user', false, false),
+  ('patient/Observation/*.read', 'Read to FHIR Observations Resources', 'user', false, false),
+  ('patient/Observation/*.write', 'Write to FHIR Observations Resources', 'user', false, false);
 
   
 --
 -- Merge the temporary scopes safely into the database. This is a two-step process to keep scopes from being created on every startup with a persistent store.
 --
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) 
-  SELECT scope, description, icon, restricted, default_scope, structured, structured_param_description FROM system_scope_TEMP
+INSERT INTO system_scope (scope, description, icon, restricted, default_scope)
+  SELECT scope, description, icon, restricted, default_scope FROM system_scope_TEMP
   ON DUPLICATE KEY UPDATE system_scope.scope = system_scope.scope;
 
 COMMIT;
--- openid-connect-server-webapp/src/main/resources/db/oracle/scopes_oracle.sql
+++ openid-connect-server-webapp/src/main/resources/db/oracle/scopes_oracle.sql
@@ -15,56 +15,56 @@ INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_sco
 INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
   ('offline_access', 'offline access', 'time', 0, 0);
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('smart', 'Details of SMART authorization', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('smart', 'Details of SMART authorization', 'user', 0, 0);
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('smart/orchestrate_launch', 'Orchestrate a launch with EHR context', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('smart/orchestrate_launch', 'Orchestrate a launch with EHR context', 'user', 0, 0);
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('launch', 'Launch with an existing context', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('launch', 'Launch with an existing context', 'user', 0, 0);
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('launch/patient', 'Launch with patient context', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('launch/patient', 'Launch with patient context', 'user', 0, 0);
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('launch/encounter', 'Launch with encounter context', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('launch/encounter', 'Launch with encounter context', 'user', 0, 0);
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('launch/resource', 'Launch with resource context', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('launch/resource', 'Launch with resource context', 'user', 0, 0);
 
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('launch/other', 'Launch with other context', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('launch/other', 'Launch with other context', 'user', 0, 0);
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('search', 'SMART on FHIR search', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('search', 'SMART on FHIR search', 'user', 0, 0);
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('summary', 'SMART on FHIR summary', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('summary', 'SMART on FHIR summary', 'user', 0, 0);
 
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('user/*.*', 'Permission to read and write all resources that the current user can access', 'user', 0, 0, 0, null);
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('user/*.*', 'Permission to read and write all resources that the current user can access', 'user', 0, 0);
 
-  INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('patient/*.*', 'Permission to read/write any resource for the current patient', 'user', 0, 0, 0, null);
+  INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('patient/*.*', 'Permission to read/write any resource for the current patient', 'user', 0, 0);
 
-  INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('patient/*.read', 'Read Permission to read any resource for the current patient', 'user', 0, 0, 0, null);
-  INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('patient/*.write', 'Write Permission to read any resource for the current patient', 'user', 0, 0, 0, null);
-  INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('patient/Patient/*.*', 'Read/Write to FHIR Patient Resources', 'user', 0, 0, 0, null);
-  INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('patient/Patient/*.read', 'Read to FHIR Patient Resources', 'user', 0, 0, 0, null);
-  INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('patient/Patient/*.write', 'Write to FHIR Patient Resources', 'user', 0, 0, 0, null);
-  INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('patient/Observation/*.*', 'Read/Write to FHIR Observations Resources', 'user', 0, 0, 0, null);
-  INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('patient/Observation/*.read', 'Read to FHIR Observations Resources', 'user', 0, 0, 0, null);
-  INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  ('patient/Observation/*.write', 'Write to FHIR Observations Resources', 'user', 0, 0, 0, null);
+  INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('patient/*.read', 'Read Permission to read any resource for the current patient', 'user', 0, 0);
+  INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('patient/*.write', 'Write Permission to read any resource for the current patient', 'user', 0, 0);
+  INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('patient/Patient/*.*', 'Read/Write to FHIR Patient Resources', 'user', 0, 0);
+  INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('patient/Patient/*.read', 'Read to FHIR Patient Resources', 'user', 0, 0);
+  INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('patient/Patient/*.write', 'Write to FHIR Patient Resources', 'user', 0, 0);
+  INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('patient/Observation/*.*', 'Read/Write to FHIR Observations Resources', 'user', 0, 0);
+  INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('patient/Observation/*.read', 'Read to FHIR Observations Resources', 'user', 0, 0);
+  INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+  ('patient/Observation/*.write', 'Write to FHIR Observations Resources', 'user', 0, 0);
 --
 -- Merge the temporary scopes safely into the database. This is a two-step process to keep scopes from being created on every startup with a persistent store.
 --
